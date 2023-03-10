bade0c2d8c596ca82a0d984dd8dc1fe8a4747ad1
Minsung Kin <71819351+MSG0217@users.noreply.github.com>
2023-03-10 20:20:13 +0900

    [level 0] Title: 몫 구하기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/lv0/120805. 몫 구하기/몫 구하기.js
@@ -1,7 +1,7 @@
 function solution(num1, num2) {
     var answer = 0;
-    if(num1>0 &&num1<=100 && num2>0 && num2<=100) {
-        answer = Math.floor(num1/num2);
+    if(num1>0 &&num1<=100 && num2>0 && num2<=100) {     // 제한조건 
+        answer = Math.floor(num1/num2);     //num1을 num2로 나눈 몫을 구합니다.
     } 
     return answer;
 }

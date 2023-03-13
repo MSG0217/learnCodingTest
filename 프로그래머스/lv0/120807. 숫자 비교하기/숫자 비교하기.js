0b9e88d764b3ab0317acc421d1b38ee51e562607
Minsung Kin <71819351+MSG0217@users.noreply.github.com>
2023-03-13 09:53:59 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.js
@@ -4,3 +4,8 @@ function solution(num1, num2) {
       else answer = -1; 
     return answer;
 }
+
+// 1. num1과 num2가 같은 지 비교
+// 1-1. 비교하려면 '비교연산자' 사용
+// 2. 같다면 1 반환, 다르면 -1 반환
+// 2-1. 조건에 따라 결과값이 다르다면 조건문(if)문 사용!

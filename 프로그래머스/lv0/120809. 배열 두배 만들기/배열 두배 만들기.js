2873fd867b5cb3a3eb03de2113ae08c072e8d366
Minsung Kin <71819351+MSG0217@users.noreply.github.com>
2023-03-13 19:39:41 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.09 ms, Memory: 33.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,7 +1,9 @@
 function solution(numbers) {
     var answer = [];
+    // 인덱스 0번부터 마지막번까지 반복하며
     for(let i = 0; i<numbers.length; i++) {
-      answer.push(numbers[i]*2);
+        // 배열 인덱스마다 두 배 곱한 것을 정답배열에 푸시하기
+        answer.push(numbers[i]*2);
     }
     return answer;
 }

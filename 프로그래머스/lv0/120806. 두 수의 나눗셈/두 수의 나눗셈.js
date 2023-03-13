4487afcdb1ebe8221ac2a155fe94f114c8b6845c
Minsung Kin <71819351+MSG0217@users.noreply.github.com>
2023-03-13 09:51:00 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.js
@@ -3,3 +3,10 @@ function solution(num1, num2) {
     answer = Math.trunc((num1/num2) * 1000);
     return answer;
 }
+
+// 1. num1을 num2로 나눈다.
+// 2. 나눈 값에 1,000을 곱한다.
+// 3. 그 값의 정수 부분을 구한다.
+// 3-1. Math.floor vs. parseInt ?
+//      parseInt는 편법 = 문자를 받아서 정수를 반환 = 숫자계산에 적합X
+//      if (num < 0) parseInt 오류발생

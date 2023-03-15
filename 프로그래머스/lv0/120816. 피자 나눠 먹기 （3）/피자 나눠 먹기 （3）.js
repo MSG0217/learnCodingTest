fbc33937496e208df0d8e136ede4168b4c3c4455
Minsung Kin <71819351+MSG0217@users.noreply.github.com>
2023-03-15 09:30:34 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,8 +1,10 @@
 function solution(slice, n) {
     let pizza = 1;
-    while (slice*pizza/n < 1) {
+    // 조각 수 * 피자 판수가 인원 수보다 적으면 while문 반복하여 피자 수 증가
+    while (slice*pizza < n) {
         pizza++;
     }
+    // 조각 수 * 피자 판수가 인원 수보다 크면 피자 판수 리턴
     return pizza;
 }
 

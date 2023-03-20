cb5e11afcfcad4fafa929cb27bf5dd3c0ec7cfeb
Minsung Kin <71819351+MSG0217@users.noreply.github.com>
2023-03-20 10:00:20 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -1,10 +1,6 @@
 function solution(my_string) {
-    //모음 배열 
-    const gather = ['a', 'e', 'i', 'o', 'u'];
-    //매개변수 배열로 변환
-    let StringArr = my_string.split('');
-    //비교하면서 모음만 빼기 (how to?)
-    StringArr = StringArr.filter(item => !gather.includes(item))
-    // 배열을 하나의 문자열로 변환하여 리턴
-    return StringArr.join('');
+    return my_string
+            .split('')      //매개변수 배열로 변환
+            .filter(item => !['a', 'e', 'i', 'o', 'u'].includes(item))      //비교하면서 모음만 빼기
+            .join('');       // 배열을 하나의 문자열로 변환하여 리턴
 }
